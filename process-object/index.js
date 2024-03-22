99041a11fdc66d8524ec12b1c323421a30ba863a
Nirwansyah Ramdhani <nirwansyah.r@kiranatama.com>
2024-03-22 21:18:48 +0700

    initial commit

---
+++ process-object/index.js
@@ -0,0 +1,18 @@
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for (let i = 0; i <= 10000; i++) {
+  // Proses looping ini akan membuat penggunaan memori naik
+  if (i === 10000) {
+    console.log("Done");
+  }
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(
+  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
+);
